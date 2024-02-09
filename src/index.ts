c06d807ca5d751d7401e038e86289043a84947b7
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-02-09 20:17:34 +0100

    base

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { concatination } from "./concatenation";
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+
+if (button && input) {
+    button.addEventListener('click', ()=>{concatination(input.value, 'hello!');
+    })
+}
